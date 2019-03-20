f55b76f32de0c588d9fa0ece347d8443c724cb14
Pablo Trinidad <pabloatp0204@gmail.com>
2019-03-20 00:01:49 -0600

    Project 01 | Lexicographic order achieved

--- projects/01/src/main/java/mx/unam/ciencias/proyecto1/LexicographicSort.java
+++ projects/01/src/main/java/mx/unam/ciencias/proyecto1/LexicographicSort.java
@@ -1,10 +1,20 @@
 package mx.unam.ciencias.edd;
 
+import mx.unam.ciencias.edd.Arreglos;
+
 /**
  * Proyecto 1: Ordenador lexicográfico
  */
 public class LexicographicSort {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        String stream[] = "Hombres necios que acusáis\n    a la mujer sin razón,\nsin ver que sois la ocasión\n    de lo mismo que culpáis.\n\nSi con ansia sin igual\n    solicitáis su desdén,\n¿por qué queréis que obren bien\n    si las incitáis al mal?\n\nCombatís su resistencia\n    y luego con gravedad\ndecís que fue liviandad\n    lo que hizo la diligencia.\n\nParecer quiere el denuedo\n    de vuestro parecer loco\nal niño que pone el coco\n    y luego le tiene miedo.\n\nQueréis con presunción necia\n    hallar a la que buscáis,\npara pretendida, Tais,\n    y en la posesión, Lucrecia.\n\n¿Qué humor puede ser más raro\n    que el que, falto de consejo,\nél mismo empaña el espejo\n    y siente que no esté claro?\n".split("\n");
+        Record[] cool = new Record[stream.length];
+        for(int i = 0; i < cool.length; i++) {
+            cool[i] = new Record(stream[i]);
+        }
+        Arreglos.quickSort(cool);
+        for (Record r : cool) {
+            System.out.println(r);
+        }
     }
 }
---
+++ projects/01/src/main/java/mx/unam/ciencias/proyecto1/Record.java
@@ -0,0 +1,48 @@
+package mx.unam.ciencias.edd;
+
+/**
+ * Record.
+ * 
+ * A record can contain any printable characters
+ * and will implement the comparable class so it
+ * can provide a sense of lexicographic order by
+ * default via the compareTo method.
+ */
+public class Record implements Comparable<Record> {
+
+    private String content;
+    private String sortableContent;
+
+    public Record(String content) {
+        this.content = content;
+        this.sortableContent = this.makeSortable(content);
+    }
+
+    private String makeSortable(String content) {
+        // System.out.println(content.toLowerCase());
+        String sortable = new String(content)
+            // Replace ñ/Ñ with the respective n
+            .replaceAll("[ñÑ]", "n")
+            // // Replace accents with simple vowels
+            .replaceAll("[ÀÁÂÃÄÅáàâäãå]", "a")
+            .replaceAll("[ÉÈÊËéèêë]", "e")
+            .replaceAll("[ÍÌÎÏíìîï]", "i")
+            .replaceAll("[ÓÒÔÖÕóòôöõ]", "o")
+            .replaceAll("[ÚÙÛÜúùûü]", "u")
+            // Remove anything that is not a character or a digit
+            .replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        return sortable;
+    }
+
+    public String getSortable() {
+        return this.sortableContent;
+    }
+
+    public int compareTo(Record record) {
+        return this.sortableContent.compareTo(record.getSortable());
+    }
+
+    public String toString() {
+        return this.content;
+    }
+}
