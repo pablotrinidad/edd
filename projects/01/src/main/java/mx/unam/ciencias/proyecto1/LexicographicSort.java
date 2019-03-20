c9c099e87bbfcc3d39061899a2de8fd7923981f0
Pablo Trinidad <pabloatp0204@gmail.com>
2019-03-19 21:55:28 -0600

    Project setup

---
+++ projects/01/src/main/java/mx/unam/ciencias/proyecto1/LexicographicSort.java
@@ -0,0 +1,10 @@
+package mx.unam.ciencias.edd;
+
+/**
+ * Proyecto 1: Ordenador lexicográfico
+ */
+public class LexicographicSort {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
