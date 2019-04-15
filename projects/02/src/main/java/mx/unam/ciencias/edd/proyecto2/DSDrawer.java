394337405513700df53378dd2a274d4497095621
Pablo Trinidad <pabloatp0204@gmail.com>
2019-04-15 16:35:35 -0500

    Project 02 | Argument parser v.1 ready

---
+++ projects/02/src/main/java/mx/unam/ciencias/edd/proyecto2/DSDrawer.java
@@ -0,0 +1,47 @@
+package mx.unam.ciencias.edd.proyecto2;
+
+/**
+ * Data Structure Drawer.
+ * 
+ * A small command line application that output the graphic representation
+ * of a defined set of data structures as SVG files. It only relies on the
+ * data structures' implementations available on mx.unam.ciencias.edd.
+ */
+public class DSDrawer {
+
+    // Provides an API to parse and access the data used to instantiate the program.
+    private ArgumentParser argsParser;
+
+    /**
+     * Application's entry point.
+     * 
+     * Create an application instance with the given command line arguments.
+     */
+    public static void main(String[] args) {
+        DSDrawer app = new DSDrawer();
+        app.draw(args);
+    }
+
+
+    /**
+     * Parse the command line arguments and call the
+     * drawDataStructure method with the parsed data.
+     */
+    public void draw(String[] args) {
+        this.argsParser = new ArgumentParser(args);
+
+        this.drawDataStructure(
+            argsParser.getDataStructure(),
+            argsParser.getData()
+        );
+    }
+
+    private void drawDataStructure(DataStructures DS, int[] data) {
+        String content;
+        switch (DS) {
+            case Queue:
+                System.out.print("Queue");
+                break;
+        }
+    }
+}
---
+++ projects/02/src/main/java/mx/unam/ciencias/edd/proyecto2/DataStructures.java
@@ -0,0 +1,14 @@
+package mx.unam.ciencias.edd.proyecto2;
+
+
+public enum DataStructures {
+    LinkedList,
+    Stack,
+    Queue,
+    BinarySearchTree,
+    CompleteBinaryTree,
+    RBTree,
+    AVLTree,
+    Graph,
+    Heap
+}
