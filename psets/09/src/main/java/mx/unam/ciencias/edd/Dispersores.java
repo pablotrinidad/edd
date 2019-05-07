5f9b351b52cf8af7f927154255f250712a692eb2
Pablo Trinidad <pabloatp0204@gmail.com>
2019-05-07 15:42:10 -0500

    P09 | XOR hash function

--- psets/09/src/main/java/mx/unam/ciencias/edd/Dispersores.java
+++ psets/09/src/main/java/mx/unam/ciencias/edd/Dispersores.java
@@ -14,7 +14,30 @@ public class Dispersores {
      * @return la dispersión de XOR de la llave.
      */
     public static int dispersaXOR(byte[] llave) {
-        // Aquí va su código.
+        byte[] k;
+        if(llave.length % 4 != 0) {
+            int l = 4 - (llave.length % 4);
+            k = new byte[llave.length + l];
+            for(int i = 0; i < k.length; i++) {
+                k[i] = i >= llave.length ? (byte) 0 : llave[i];
+            }
+
+        } else { k = llave;}
+
+        int r = 0;
+
+        for(int i = 0; i < k.length / 4; i ++) {
+            int m = i * 4;
+            int n = (
+                (k[m] & 0xFF) << 24 |
+                (k[m+1] & 0xFF) << 16 |
+                (k[m+2] & 0xFF) << 8 |
+                (k[m+3] & 0xFF)
+            );
+            r ^= n;
+        }
+        return r;
+
     }
 
     /**
@@ -23,7 +46,7 @@ public class Dispersores {
      * @return la dispersión de Bob Jenkins de la llave.
      */
     public static int dispersaBJ(byte[] llave) {
-        // Aquí va su código.
+        return 1;
     }
 
     /**
@@ -32,6 +55,7 @@ public class Dispersores {
      * @return la dispersión de Daniel Bernstein de la llave.
      */
     public static int dispersaDJB(byte[] llave) {
-        // Aquí va su código.
+        return 1;
     }
+
 }
